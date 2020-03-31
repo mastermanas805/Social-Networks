37b315041dd17b3d81b93b9710c98b595dc760df
manas srivastava <manas10@live.com>
2020-03-31 22:24:36 +0530

    schelling

--- Social Networks/week5/Schellings_Model.py
+++ Social Networks/week5/Schellings_Model.py
@@ -19,6 +19,63 @@ def display_graph(G):
     nx.draw_networkx_labels(G,pos,labels = labels)
     plt.show()
 
+def get_neigh_for_internal(u,v):
+    return [(u-1,v),(u+1,v),(u,v-1),(u,v+1),(u-1,v+1),(u+1,v-1),(u-1,v-1),(u+1,v+1)]
+
+def get_neigh_for_boundary(u,v):
+
+    if u == 0 and v == 0:
+        return [(0,1), (1,1), (1,0)]
+
+    elif u == 1 and v == N-1:
+        return [(N-2,N-2),(N-1,N-2),(N-2,N-1)]
+
+    elif u == N-1 and v == 0:
+        return [(u-1,v), (u,v+1), (u-1, v+1)]
+
+    elif u == 0 and v == N-1:
+        return [(u+1,v) , (u+1,v-1), (u,v-1)]
+
+    elif u == 0:
+        return [(u,v-1) , (u,v+1), (u+1,v), (u+1,v-1),(u+1,v+1)]
+
+    elif u == N-1:
+        return [(u-1,v) , (u,v-1), (u,v+1), (u-1,v+1), (u-1,v-1)]
+
+    elif v == N-1:
+        return [(u,v-1), (u-1,v), (u+1,v), (u-1,v-1), (u+1,v-1)]
+
+    elif v == 0:
+        return [(u-1,v) , (u+1,v), (u,v+1), (u-1,v+1), (u+1,v+1)]
+
+def get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list):
+    unsatisfied_nodes_list = []
+    t= 3
+
+    for u,v in G.nodes():
+        type_of_this_node = G.nodes[(u,v)]['type']
+        print(type_of_this_node)
+
+        if type_of_this_node == 0:
+            continue
+
+        else:
+            try:
+                similar_nodes = 0
+                if (u,v) in internal_nodes_list:
+                    neigh = get_neigh_for_internal(u, v)
+                elif (u,v) in boundary_nodes_list:
+                    neigh = get_neigh_for_boundary(u,v)
+
+                for each in neigh:
+                    #print(G.nodes[each]['type'],type_of_this_node)
+                    if G.nodes[each]['type'] == type_of_this_node:
+                        similar_nodes += 1
+                if similar_nodes <= t:
+                    unsatisfied_nodes_list.append((u,v))
+            except:
+                pass
+    return unsatisfied_nodes_list
 
 def get_boundary_nodes(G):
     boundary_nodes_list = []
@@ -55,3 +112,5 @@ display_graph(G)
 
 boundary_nodes_list = get_boundary_nodes(G)
 internal_nodes_list = list(set(G.nodes()) - set(boundary_nodes_list))
+unsatisfied_nodes_list = get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list)
+print(unsatisfied_nodes_list)
